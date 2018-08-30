6ea068f70d920cf62f492ee3684cf24754550c80
Jake Duff <42357602+jaked3@users.noreply.github.com>
2018-08-30 16:19:10 -0400

    server linked to front end

--- Server.js
+++ Server.js
@@ -73,7 +73,8 @@ app.post('/send', (req, res) => {
       console.log('Message sent: %s', info.messageId);   
       console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-      res.render('contact', {msg:'Email has been sent'});
+      //res.render('contact', {msg:'Email has been sent'});
+      res.json('success');
   });
   });
 
